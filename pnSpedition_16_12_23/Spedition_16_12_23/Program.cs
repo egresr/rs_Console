44b9cd8375a956b242000166af07e5933c06e836
egresr <rs1@bk.ru>
2016-12-23 00:24:56 +0100

    ***Start ___ Spedition_16_12_23

---
+++ pnSpedition_16_12_23/Spedition_16_12_23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spedition_16_12_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

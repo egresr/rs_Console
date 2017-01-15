c50c9a164eb6aace1a294b21b648509b12a4b81a
egresr <rs1@bk.ru>
2017-01-15 23:43:33 +0100

    ---Parkhaus Waende hinzugefuegt

---
+++ Parkhaus_17_01_15/Parkhaus/ParkhausRaum.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkhaus
+{
+    class ParkhausRaum
+    {
+        int abstandVonOben;
+        int abstandVonUnten;
+        int abstandVonLinks;
+        int abstandVonRechts;
+
+        Position eckeLinksOben;
+        Position eckeLinksUnten;
+        Position eckeRechtsOben;
+        Position eckeRechtsUnten;
+
+        public int EckeLinksOben { get { return eckeLinksOben.KoordinateX; } }
+        public int EckeLinksUnten { get { return eckeLinksUnten.KoordinateX; } }
+        public int EckeRechtsOben { get { return eckeRechtsOben.KoordinateX; } }
+        public int EckeRechtsUnten { get { return eckeRechtsUnten.KoordinateX; } }
+
+        public ParkhausRaum(int abstandVonOben, int abstandVonUnten, int abstandVonLinks, int abstandVonRechts)
+        {
+            this.abstandVonOben = abstandVonOben;
+            this.abstandVonUnten = abstandVonUnten;
+            this.abstandVonLinks = abstandVonLinks;
+            this.abstandVonRechts = abstandVonRechts;
+
+            BestimmePosition_EckeLinksOben();
+            BestimmePosition_EckeLinksUnten();
+            BestimmePosition_EckeRechtsOben();
+            BestimmePosition_EckeRechtsUnten();
+
+            ZeichneWandOben();
+            ZeichneWandUnten();
+            ZeichneWandLinks();
+            ZeichneWandRechts();
+
+
+        }
+
+        private void BestimmePosition_EckeLinksOben()
+        {
+            int koordinateX = abstandVonLinks;
+            int koordinateY = abstandVonOben;
+
+            eckeLinksOben = new Position(koordinateX, koordinateY);
+        }
+
+        private void BestimmePosition_EckeRechtsOben()
+        {
+            int koordinateX = Console.WindowWidth - abstandVonRechts - 1;
+            int koordinateY = abstandVonOben;
+
+            eckeRechtsOben = new Position(koordinateX, koordinateY);
+        }
+
+        private void BestimmePosition_EckeLinksUnten()
+        {
+            int koordinateX = abstandVonLinks;
+            int koordinateY = Console.WindowHeight - abstandVonUnten - 1;
+
+            eckeLinksUnten = new Position(koordinateX, koordinateY);
+        }
+
+        private void BestimmePosition_EckeRechtsUnten()
+        {
+            int koordinateX = Console.WindowWidth - abstandVonRechts - 1;
+            int koordinateY = Console.WindowHeight - abstandVonUnten - 1;
+
+            eckeRechtsUnten = new Position(koordinateX, koordinateY);
+        }
+
+        private void ZeichneWandOben()
+        {
+            int laenge = eckeRechtsOben.KoordinateX - eckeLinksOben.KoordinateX;
+            Wand wandOben = new Wand(eckeLinksOben.KoordinateX, eckeLinksOben.KoordinateY, laenge, Richtung.nachRechts);
+            wandOben.Zeichne();
+        }
+
+        private void ZeichneWandUnten()
+        {
+            int laenge = eckeRechtsOben.KoordinateX - eckeLinksOben.KoordinateX;
+            Wand wandUnten = new Wand(eckeLinksUnten.KoordinateX, eckeLinksUnten.KoordinateY, laenge, Richtung.nachRechts);
+            wandUnten.Zeichne();
+        }
+
+        private void ZeichneWandLinks()
+        {
+            int laenge = eckeLinksUnten.KoordinateY - eckeLinksOben.KoordinateY;
+            Wand wandLinks = new Wand(eckeLinksOben.KoordinateX, eckeLinksOben.KoordinateY, laenge, Richtung.nachUnten);
+            wandLinks.Zeichne();
+        }
+
+        private void ZeichneWandRechts()
+        {
+            int laenge = eckeRechtsUnten.KoordinateY - eckeRechtsOben.KoordinateY + 1;
+            Wand wandRechts = new Wand(eckeRechtsOben.KoordinateX, eckeRechtsOben.KoordinateY, laenge, Richtung.nachUnten);
+            wandRechts.Zeichne();
+        }
+    }
+}
--- Parkhaus_17_01_15/Parkhaus/Program.cs
+++ Parkhaus_17_01_15/Parkhaus/Program.cs
@@ -10,40 +10,14 @@ namespace Parkhaus
     {
         static void Main(string[] args)
         {
-            Wand wandHorizontal = new Wand(3, 1, 150, Richtung.nachRechts);
-            Wand wandHorizontal1 = new Wand(3, 1, 30, Richtung.nachUnten);
-            wandHorizontal.Zeichne();
-            wandHorizontal1.Zeichne();
+            //Wand wandHorizontal = new Wand(3, 1, 150, Richtung.nachRechts);
+            //Wand wandHorizontal1 = new Wand(3, 1, 30, Richtung.nachUnten);
+            //wandHorizontal.Zeichne();
+            //wandHorizontal1.Zeichne();
+
+            ParkhausRaum parkhausRaum = new ParkhausRaum(2, 1, 1, 1);
 
             Console.ReadLine();
         }
     }
 }
-
-//////Mauerstein mauerstein = new Mauerstein(1, 1);
-//////Mauerstein mauerstein1 = new Mauerstein(2, 1);
-//////Mauerstein mauerstein2 = new Mauerstein(1, 2);
-//////Mauerstein mauerstein3 = new Mauerstein(2, 2);
-//////Mauerstein mauerstein4 = new Mauerstein(1, 3);
-//////Mauerstein mauerstein5 = new Mauerstein(2, 3);
-
-//////Mauerstein mauerstein6 = new Mauerstein(3, 1);
-//////Mauerstein mauerstein7 = new Mauerstein(4, 1);
-//////Mauerstein mauerstein8 = new Mauerstein(5, 1);
-//////Mauerstein mauerstein9 = new Mauerstein(6, 1);
-//////Mauerstein mauerstein10 = new Mauerstein(7, 1);
-//////Mauerstein mauerstein11 = new Mauerstein(8, 1);
-
-//////mauerstein.Zeichne();
-//////mauerstein1.Zeichne();
-//////mauerstein2.Zeichne();
-//////mauerstein3.Zeichne();
-//////mauerstein4.Zeichne();
-//////mauerstein5.Zeichne();
-
-//////mauerstein6.Zeichne();
-//////mauerstein7.Zeichne();
-//////mauerstein8.Zeichne();
-//////mauerstein9.Zeichne();
-//////mauerstein10.Zeichne();
-//////mauerstein11.Zeichne();
